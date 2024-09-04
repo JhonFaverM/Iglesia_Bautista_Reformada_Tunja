5b80d121f77bc5a522cd78fa6028d6b3f39aa1f7
Jhon Fa <jhonfaverm@gmail.com>
2024-09-04 11:54:02 -0500

    Ajustando el login, sin formularioreactivo

--- FRONTEND/pagina_iglesia/src/app/components/login/login.component.ts
+++ FRONTEND/pagina_iglesia/src/app/components/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
@@ -11,58 +10,40 @@ import Swal from 'sweetalert2';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup;
-
-  constructor(
-    private fb: FormBuilder,
-    private authService: AuthService,
-    private routerService: Router
-  ) { 
-    // Inicializar el formulario reactivo
-    this.loginForm = this.fb.group({
-      usuario: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    });
+  datosLogin = {
+    usuario: "",
+    password: ""
   }
 
-  ngOnInit(): void {}
+  constructor(private authService: AuthService, private routerService: Router) { }
+
+  ngOnInit(): void {
+  }
 
   login() {
-    if (this.loginForm.valid) {
-      this.authService.loginUser(this.loginForm.value).subscribe({
-        next: (res) => {
-          if ((res as any).token) {
-            localStorage.setItem('token', (res as any).token);
-            this.routerService.navigate(['/gestionUno']);
-            this.loginForm.reset();
-          } else {
-            Swal.fire({
-              title: 'Error',
-              text: (res as any).msg,
-              icon: 'error',
-              confirmButtonText: 'Aceptar'
-            });
-          }
-        },
-        error: () => {
+    this.authService.loginUser(this.datosLogin).subscribe({
+      next: (res) => {
+        if ((res as any).token) {
+          localStorage.setItem('token', (res as any).token);
+          this.routerService.navigate(['/gestionUno']);
+        } else {
           Swal.fire({
             title: 'Error',
-            text: 'Error en la conexión con el servidor',
+            text: (res as any).msg, // Mostrar el mensaje específico del backend
             icon: 'error',
             confirmButtonText: 'Aceptar'
           });
-        },
-        complete: () => {
-        this.loginForm.reset();
         }
-      });
-    } else {
-      Swal.fire({
-        title: 'Error',
-        text: 'Formulario inválido, por favor verifica tus datos',
-        icon: 'error',
-        confirmButtonText: 'Aceptar'
-      });
-    }
+      },
+      error: () => {
+        Swal.fire({
+          title: 'Error',
+          text: 'Error en la conexión con el servidor',
+          icon: 'error',
+          confirmButtonText: 'Aceptar'
+        });
+      }
+    });
   }
+
 }
